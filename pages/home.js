0606288ede5ac76053f020b07357c7b4a1b90084
Deren <derenlin123456@gmail.com>
2021-09-14 15:14:17 -0400

    homepage changes

--- pages/home.js
+++ pages/home.js
@@ -1,18 +1,13 @@
 import React from 'react'
 
-import React from 'react'
+const Home = () => (
+  <div>
+    <img src="https://user-images.githubusercontent.com/2351721/31314483-7611c488-ac0e-11e7-97d1-3cfc1c79610e.png" alt="placeholder" width="690" height="420"/>
+    <h1>Reservify</h1>
+    <a>Cart</a><br/>
+    <a>Help</a><br/>
+    <a>About</a><br/>
+  </div>
+)
 
-class Home extends Component {
-  render() {
-      return(
-          <div>
-              <img src="https://user-images.githubusercontent.com/2351721/31314483-7611c488-ac0e-11e7-97d1-3cfc1c79610e.png" alt="placeholder" width="690" height="420"/>
-              <h1>Reservify</h1>
-              <Link to="/Cart">Cart</Link><br/>
-              <Link to="/Help">Help</Link><br/>
-              <Link to="/About">About</Link><br/>
-          </div>
-      );
-  }
-}
 export default Home;
