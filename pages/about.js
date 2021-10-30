9dc48537c81b76133dc4b19864b85c50a92364ce
Hayley Robinson <hayley.robinson63@myhunter.cuny.edu>
2021-10-30 11:36:16 -0400

    initial about page

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,35 @@
 import React from 'react'
 import Header from '../components/header'
 import Navbar from '../components/navbar'
+import Description from '../components/description'
 
 const About = () => (
-  <div styles={positionStyle}>
+  <div style={positionStyle}>
     <Header />
     <Navbar/>
-    <div styler ={header}>About us</div>
+    <div>
+      <div style ={header}>
+        About Us
+      </div>
+      <Description/>
+    </div>
   </div >
 )
 
 const header = {
+  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(https://images.unsplash.com/photo-1596873364163-c3b61fa24af7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80)`,
+  height: 300,
+  width: '100%',
+  objectFit: 'contain',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+
+  textShadow: '-1px -1px 0 ##F3F7F0, 1px -1px 0 #F3F7F0, -1px 1px 0 #000, 1px 1px 0 #000',
+  color: "#F3F7F0",
+  fontSize: "45px",
   display: 'flex',
+  alignItems: 'center',
   justifyContent: 'center',
-  color: "black",
-  fontSize: "22px"
 }
 
 const positionStyle = {
