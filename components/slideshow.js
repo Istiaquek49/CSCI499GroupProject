4436078012bfb705b29bf4dbf4aba65bd7dc1365
Istiaque Khan <Istiaque.Khan49@myhunter.cuny.edu>
2021-10-31 23:09:07 -0400

    minor change in text for pic
    *dessert*

--- components/slideshow.js
+++ components/slideshow.js
@@ -13,7 +13,7 @@ const fadeImages = [
   },
   {
     url: 'https://images.unsplash.com/photo-1623888885364-56cf1a38b6ad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
-    caption: 'Dessert for two'
+    caption: 'Dessert'
   },
   {
     url: 'https://images.unsplash.com/photo-1595596586764-ebf88a40e690?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
